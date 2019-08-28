1d8650be48a36b182e2b95c4be44e2a41e679be5
danish-jamil <mrdanishjameel@gmail.com>
2019-08-28 17:31:38 +0500

    Auto translate using google translate api for languages other than base language

--- src/lib.ts
+++ src/lib.ts
@@ -1,88 +1,141 @@
-import { window, workspace } from "vscode";
-import fs = require("fs");
-import path = require("path");
+import { window, workspace } from 'vscode';
+import fs = require('fs');
+import path = require('path');
+import { languages } from './languages';
+const translate = require('google-translate-open-api').default;
 
-let dotProp = require("dot-prop-immutable");
+let dotProp = require('dot-prop-immutable');
 
 export abstract class GenerateTranslation {
   private static _editor = window.activeTextEditor;
-
-  public static generate(key: string) {
-    GenerateTranslation.fromSelectedText(key);
+  public static generate(key: string, translation?: string) {
+    GenerateTranslation.fromSelectedText(key, translation);
   }
 
-  public static async fromSelectedText(textSelection: string) {
+  public static detectLanguage(filePath: string) {
+    const startIndex = filePath.lastIndexOf('\\');
+    const fileName = filePath.substr(startIndex + 1, 2);
+    const lang = languages.find(lang => fileName.includes(lang.value));
+    if (lang) {
+      return lang.value;
+    }
+    return null;
+  }
+
+  public static async fromSelectedText(
+    textSelection: string,
+    translation?: string
+  ) {
     try {
       const path = workspace
-        .getConfiguration("generate-translation")
-        .get("path");
-
+        .getConfiguration('generate-translation')
+        .get('path');
+      const preferredLanguage =
+        workspace
+          .getConfiguration('generate-translation')
+          .get('preferredLanguage') || 'en';
       let pathToFind = `${workspace.rootPath}${path}`;
       const translateFiles = GenerateTranslation.getFiles(
         pathToFind,
-        ".json",
+        '.json',
         null,
         []
       );
 
+      translateFiles.sort((a, b) =>
+        this.detectLanguage(a) === preferredLanguage ? -1 : 1
+      );
+      const preferredLanguageFile = translateFiles.shift() as string;
+      this.generateTranslation(
+        preferredLanguageFile,
+        pathToFind,
+        textSelection,
+        translation as string,
+        preferredLanguage as string
+      );
+
       for (let i = 0; i < translateFiles.length; i++) {
         const file = translateFiles[i];
-        let translateObject = JSON.parse(fs.readFileSync(file, "utf-8"));
-
-        const translateObjectName = file.replace(`${pathToFind}/`, "");
-
-        if (dotProp.get(translateObject, textSelection)) {
-          window.showErrorMessage(
-            `${textSelection} already exists in the file ${translateObjectName}.`
-          );
-        } else {
-          const value = await window.showInputBox({
-            prompt: `What is value in ${translateObjectName} ?`,
-            placeHolder: textSelection
-          });
-
-          if (value) {
-            const arrTextSelection = textSelection.split(".");
-            arrTextSelection.pop();
-
-            const valueLastKey = dotProp.get(
-              translateObject,
-              arrTextSelection.join(".")
-            );
-            if (valueLastKey && typeof valueLastKey === "string") {
-              const newObject = {
-                [arrTextSelection[arrTextSelection.length - 1]]: valueLastKey
-              };
-
-              translateObject = dotProp.set(
-                translateObject,
-                arrTextSelection.join("."),
-                newObject
-              );
-            }
-
-            translateObject = dotProp.set(
-              translateObject,
-              GenerateTranslation.normalizeKey(textSelection),
-              value
-            );
-
-            await GenerateTranslation.updateFile(
-              file,
-              translateObject,
-              translateObjectName
-            );
-
-            window.showInformationMessage(
-              `${textSelection} added in the file ${translateObjectName}.`
-            );
-
-            GenerateTranslation.replaceOnTranslate(textSelection);
-          }
-        }
+        this.generateTranslation(
+          file,
+          pathToFind,
+          textSelection,
+          translation as string,
+          preferredLanguage as string
+        );
       }
     } catch (error) {
       window.showErrorMessage(error.message);
+    }
+  }
+
+  static async generateTranslation(
+    file: string,
+    pathToFind: string,
+    textSelection: string,
+    translation: string,
+    preferredLanguage: string
+  ) {
+    let translateObject = JSON.parse(fs.readFileSync(file, 'utf-8'));
+
+    const translateObjectName = file.replace(`${pathToFind}`, '');
+
+    const language = this.detectLanguage(file);
+
+    if (dotProp.get(translateObject, textSelection)) {
+      window.showErrorMessage(
+        `${textSelection} already exists in the file ${translateObjectName}.`
+      );
+    } else {
+      let value = '';
+      if (translation && language === preferredLanguage) {
+        value = translation;
+      } else {
+        const res = await translate(translation, {
+          from: preferredLanguage,
+          to: language
+        });
+        value = res.data[0];
+      }
+
+      if (value) {
+        const arrTextSelection = textSelection.split('.');
+        arrTextSelection.pop();
+
+        const valueLastKey = dotProp.get(
+          translateObject,
+          arrTextSelection.join('.')
+        );
+        if (valueLastKey && typeof valueLastKey === 'string') {
+          const newObject = {
+            [arrTextSelection[arrTextSelection.length - 1]]: valueLastKey
+          };
+
+          translateObject = dotProp.set(
+            translateObject,
+            arrTextSelection.join('.'),
+            newObject
+          );
+        }
+
+        translateObject = dotProp.set(
+          translateObject,
+          GenerateTranslation.normalizeKey(textSelection),
+          value
+        );
+
+        await GenerateTranslation.updateFile(
+          file,
+          translateObject,
+          translateObjectName
+        );
+
+        window.showInformationMessage(
+          `${textSelection} added in the file ${translateObjectName}.`
+        );
+
+        GenerateTranslation.replaceOnTranslate(textSelection);
+      }
     }
   }
 
@@ -90,26 +143,26 @@ export abstract class GenerateTranslation {
     const editor = window.activeTextEditor;
     const replaceForExtensions = <Array<string>>(
       workspace
-        .getConfiguration("generate-translation")
-        .get("replaceForExtensions")
+        .getConfiguration('generate-translation')
+        .get('replaceForExtensions')
     );
     const templateSnippetToReplace = <string>(
       workspace
-        .getConfiguration("generate-translation")
-        .get("templateSnippetToReplace")
+        .getConfiguration('generate-translation')
+        .get('templateSnippetToReplace')
     );
 
     const extname = path.extname(editor.document.fileName);
 
     if (
       editor &&
-      replaceForExtensions.indexOf(extname.replace(".", "")) > -1 &&
+      replaceForExtensions.indexOf(extname.replace('.', '')) > -1 &&
       templateSnippetToReplace
     ) {
       editor.edit(editBuilder => {
         editBuilder.replace(
           editor.selection,
-          templateSnippetToReplace.replace("i18n", textSelection)
+          templateSnippetToReplace.replace('i18n', textSelection)
         );
       });
     }
@@ -130,8 +183,8 @@ export abstract class GenerateTranslation {
       }
 
       const sort = workspace
-        .getConfiguration("generate-translation")
-        .get("sort");
+        .getConfiguration('generate-translation')
+        .get('sort');
       if (sort) {
         translateObject = GenerateTranslation.sortObject(translateObject);
       }
@@ -155,7 +208,7 @@ export abstract class GenerateTranslation {
     ext: string,
     files: any,
     result: any[]
-  ): any => {
+  ): string[] => {
     try {
       files = files || fs.readdirSync(basePath);
       result = result || [];
@@ -177,7 +230,7 @@ export abstract class GenerateTranslation {
       return result;
     } catch {
       throw new Error(
-        "No translation file was found. Check the path configured in the extension."
+        'No translation file was found. Check the path configured in the extension.'
       );
     }
   };
@@ -198,7 +251,7 @@ export abstract class GenerateTranslation {
   };
 
   private static isPlainObject = (object: any): boolean =>
-    "[object Object]" === Object.prototype.toString.call(object);
+    '[object Object]' === Object.prototype.toString.call(object);
 
-  private static normalizeKey = (key: string) => key.replace(" ", "_");
+  private static normalizeKey = (key: string) => key.replace(' ', '_');
 }
